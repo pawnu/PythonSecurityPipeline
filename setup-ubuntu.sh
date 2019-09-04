c11ffcd916860eac8fbedb130c1b8aeeb0784c3e
pawan <pawnu@users.noreply.github.com>
2019-09-04 13:10:15 +0100

    Update setup-ubuntu.sh

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-echo "Installing git and docker"
-apt-get update && apt-get install \
-	curl \
-	git 
+echo "Installing curl and docker"
+apt-get update && apt-get install curl
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 usermod -aG docker ubuntu
