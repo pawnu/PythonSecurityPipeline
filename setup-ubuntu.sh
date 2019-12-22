f52dd7f278ac9ac398db73886ce268c14cf5755c
pawan <pawnu@users.noreply.github.com>
2019-12-22 11:48:10 +0000

    enable docker upon restart

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-echo "Installing curl and docker"
+
 apt-get update
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
+apt install docker.io -y
 apt-get install -y docker-compose
 mkdir ~/jenkins_home
+# give same level of ownership for local and remote mount file
 chown 1000 ~/jenkins_home
 usermod -aG docker ubuntu
+
+#let docker run when server is restarted
+systemctl start docker
+systemctl enable docker
+
 echo "Rebooting the Ubuntu server"
 sleep 5s
 systemctl reboot
