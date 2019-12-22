48cdee46eca542050c9722f88f25b1acfd0662a9
pawan <pawnu@users.noreply.github.com>
2019-12-22 08:27:58 +0000

    Update controlserver.sh

--- ansible/controlserver.sh
+++ ansible/controlserver.sh
@@ -2,12 +2,13 @@
 
 # Install ansible in AWS ubuntu with the following 
 sudo apt update
-sudo apt install software-properties-common
+sudo apt install software-properties-common -y
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
 
 # Generate a keypair id_rsa (priv key), id_rsa.pub (public key)
-ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+# If the files already exist then it will give error, ignore it by or clause
+ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa || true
 
 # copy the content of id_rsa.pub to your target host machines's authorised_key file
 # authorised_key file is in the same ~/.ssh/ directory of host
