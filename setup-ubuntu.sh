bb9bacfdafa49894b8d15b64fb2b9a27a5289eb7
pawan <pawnu@users.noreply.github.com>
2019-12-23 23:33:26 +0000

    Update setup-ubuntu.sh

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-apt-get update
-apt install docker.io -y
-apt-get install -y docker-compose
-mkdir ~/jenkins_home
-# give same level of ownership for local and remote mount file
-chown 1000 ~/jenkins_home
-usermod -aG docker ubuntu
+sudo apt-get update
+sudo apt install docker.io -y
+sudo apt-get install -y docker-compose
+sudo usermod -aG docker ubuntu
+#restart group
+sudo newgrp docker
 
 #let docker run when server is restarted
 systemctl start docker
 systemctl enable docker
 
-echo "Rebooting the Ubuntu server"
-sleep 5s
-systemctl reboot
+#build the jenkins container
+docker-compose up -d --build
