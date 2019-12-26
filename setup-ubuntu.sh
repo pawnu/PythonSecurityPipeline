35c8ac06ee33413b936e47ee29cc5a586027dfa9
pawan <pawnu@users.noreply.github.com>
2019-12-26 15:54:52 +0000

    use wget instead of curl

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -15,11 +15,15 @@ systemctl enable docker
 #create random password for jenkins user which will be created automatically
 export Jenkins_PW=$(openssl rand -base64 16)
 export JAVA_OPTS="-Djenkins.install.runSetupWizard=false"
+echo "------- Your temporary PW ---------"
+echo $Jenkins_PW
 
 #build the jenkins container
 docker-compose up -d --build
 
 #Create the jenkins job
 apt install default-jre -y
-curl 'localhost:8080/jnlpJars/jenkins-cli.jar' > jenkins-cli.jar
-java -jar jenkins-cli.jar -s http://localhost:8080 -auth myjenkins:$Jenkins_PW create-job pythonpipeline < config.xml
+sleep 5
+wget http://localhost:8080/jnlpJars/jenkins-cli.jar
+sleep 5
+java -jar ./jenkins-cli.jar -s http://localhost:8080 -auth myjenkins:$Jenkins_PW create-job pythonpipeline < config.xml
