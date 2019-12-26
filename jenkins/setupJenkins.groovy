5b4182b505c5a2bae1ee7a1120b53ac919edb03c
pawan <pawnu@users.noreply.github.com>
2019-12-26 19:51:13 +0000

    Update and rename createuser.groovy to setupJenkins.groovy

--- jenkins/setupJenkins.groovy
+++ jenkins/setupJenkins.groovy
@@ -1,12 +1,30 @@
 import jenkins.model.*
 import hudson.security.*
+import jenkins.security.s2m.AdminWhitelistRule
+import jenkins.model.Jenkins
+import hudson.security.csrf.DefaultCrumbIssuer
+
+//don't let slave instance kill the master
+Jenkins.instance.getInjector().getInstance(AdminWhitelistRule.class)
+.setMasterKillSwitch(false)
 
 def env = System.getenv()
 
+/*
+Jenkins needs URL in order for reference links to other resources, pipeline build etc
+*/
 jlc = JenkinsLocationConfiguration.get()
 jlc.setUrl("http://"+env.JenkinsPublicIp +":8080/")
 jlc.save()    
 
+//CSFR token
+def instance = Jenkins.instance
+instance.setCrumbIssuer(new DefaultCrumbIssuer(true))
+instance.save()
+
+/*
+Create admin user to login
+*/
 def jenkins = Jenkins.getInstance()
 if(!(jenkins.getSecurityRealm() instanceof HudsonPrivateSecurityRealm))
     jenkins.setSecurityRealm(new HudsonPrivateSecurityRealm(false))
